f8daf78ede292f4e2ce674fbce8d9326cd2b1d15
RAJAT_VERMA <rv5393982@gmail.com>
2024-09-29 15:38:14 +0530

    port change for render

--- app.py
+++ app.py
@@ -67,6 +67,5 @@ def admin_data():
     return render_template('admin_data.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
+    # app.run(debug=True)
